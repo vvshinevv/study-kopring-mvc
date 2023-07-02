f908ea37094c7c3e4481534b390d91d49eee6e57
최홍희 <vvshinevv@gmail.com>
2023-07-03 00:37:44 +0900

    mapCatching -> flatMap으로 수정

--- src/main/kotlin/kopring/mvc/transaction/second/generator/IdBasedKeyGenerator.kt
+++ src/main/kotlin/kopring/mvc/transaction/second/generator/IdBasedKeyGenerator.kt
@@ -1,12 +1,13 @@
 package kopring.mvc.transaction.second.generator
 
+import arrow.core.flatMap
+import kopring.mvc.transaction.second.domain.Member
+import kopring.mvc.transaction.second.domain.MemberRepository
 import org.springframework.cache.interceptor.KeyGenerator
 import org.springframework.cache.interceptor.SimpleKey
 import org.springframework.cache.interceptor.SimpleKeyGenerator
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
-import kopring.mvc.transaction.second.domain.Member
-import kopring.mvc.transaction.second.domain.MemberRepository
 import java.lang.reflect.Method
 import java.util.Optional
 
@@ -17,7 +18,7 @@ class IdBasedKeyGenerator(private val repository: MemberRepository) : KeyGenerat
     override fun generate(target: Any, method: Method, vararg params: Any?): Any {
         return runCatching { params[0] as Long }
             .mapCatching(repository::findById)
-            .mapCatching(::generateKey)
+            .flatMap(::generateKey)
     }
 
     fun generateKey(entity: Optional<Member>, vararg params: Any): Result<Any> {
