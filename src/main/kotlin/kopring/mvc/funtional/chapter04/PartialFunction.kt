0fa2e5436056d19034dc70eb0f91958422cc6f7a
최홍희 <vvshinevv@gmail.com>
2024-11-13 01:47:30 +0900

    커링 추상화 작업

---
+++ src/main/kotlin/kopring/mvc/funtional/chapter04/PartialFunction.kt
@@ -0,0 +1,28 @@
+package kopring.mvc.funtional.chapter04
+
+class PartialFunction<in P, out R>(private val condition: (P) -> Boolean, private val f: (P) -> R) : (P) -> R {
+    override fun invoke(p: P): R = when {
+        condition(p) -> f(p)
+        else -> throw IllegalArgumentException("$p is not allowed")
+    }
+
+    fun isDefinedAt(p: P): Boolean = condition(p)
+}
+
+fun <P, R> ((P) -> R).toPartialFunction(definedAt: (P) -> Boolean): PartialFunction<P, R> =
+    PartialFunction(definedAt, this)
+
+
+fun <P1, P2, R> ((P1, P2) -> R).partial1(p1: P1): (P2) -> R {
+    return { p2 -> this(p1, p2) }
+}
+
+fun <P1, P2, R> ((P1, P2) -> R).partial2(p2: P2): (P1) -> R {
+    return { p1 -> this(p1, p2) }
+}
+
+// 커링 추상화
+fun <P1, P2, P3, R> ((P1, P2, P3) -> R).curried(): (P1) -> (P2) -> (P3) -> R = { p1: P1 ->
+    { p2: P2 -> { p3: P3 -> this(p1, p2, p3) } }
+}
+
---
+++ src/test/kotlin/kopring/mvc/functionalprogramming/chapter02/Chapter04Test.kt
@@ -0,0 +1,69 @@
+package kopring.mvc.functionalprogramming.chapter02
+
+import kopring.mvc.funtional.chapter04.PartialFunction
+import kopring.mvc.funtional.chapter04.toPartialFunction
+import mu.KotlinLogging
+import org.junit.jupiter.api.Test
+
+class Chapter04Test {
+
+    @Test
+    fun exercise04_1() {
+        fun twice(x: Int) = x * 2
+
+        // x 는 twice 함수의 부분함수이다.
+        fun partialTwice(x: Int) = if (x < 100) x * 2 else throw IllegalArgumentException()
+    }
+
+    @Test
+    fun exercise04_2() {
+        val condition: (Int) -> Boolean = { it in 1..3 }
+        val body: (Int) -> String = {
+            when (it) {
+                1 -> "One"
+                2 -> "Two"
+                3 -> "Three"
+                else -> throw IllegalArgumentException()
+            }
+        }
+
+        val oneTwoThree = PartialFunction(condition, body)
+        if (oneTwoThree.isDefinedAt(3)) {
+            log.info { oneTwoThree(3) }
+        } else {
+            log.info { "isDefinedAt(x) return false" }
+        }
+    }
+
+    @Test
+    fun exercise04_3() {
+        val condition: (Int) -> Boolean = { it.rem(2) == 0 }
+        val body: (Int) -> String = { "$it is even" }
+
+        val isEven = body.toPartialFunction(condition)
+
+        if (isEven.isDefinedAt(100)) {
+            log.info { isEven(100) }
+        } else {
+            log.info { "isDefinedAt(x) return false" }
+        }
+    }
+
+    @Test
+    fun exercise04_4() {
+
+        // 커링
+        fun multiThree(a: Int) = { b: Int -> { c: Int -> a * b * c } }
+
+        val partial1 = multiThree(1)
+        val partial2 = partial1(2)
+        val partial3 = partial2(3)
+
+        log.info { partial3 }
+        log.info { multiThree(1)(2)(3) }
+    }
+
+    companion object {
+        private val log = KotlinLogging.logger { }
+    }
+}
